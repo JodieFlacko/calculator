c9d74caca5dd3e8ecfc476ad7b2801e46dadd42f
crlyflacko <peppociccone13@gmail.com>
2024-11-27 16:59:29 +0100

    feat: functions for basic math operators

---
+++ script.js
@@ -0,0 +1,20 @@
+const add = function(x, y) {
+    return x + y;
+  };
+  
+  const subtract = function(x, y) {
+    return x - y;
+  };
+  
+  const sum = function(array) {
+    return array.reduce((total, item) =>{
+      return total + item;
+    }, 0)
+  };
+  
+  const multiply = function(array) {
+    return array.reduce((total, item) =>{
+      return total * item;
+    })
+  };
+  
