e5eb4c82464e39834b2d0d59fb401c0f26c2e398
crlyflacko <peppociccone13@gmail.com>
2024-11-27 17:59:45 +0100

    add: functions that popolate the calculator

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 // Calculator variables
 let num1, num2, operator;
+function operate(num1, num2, operator){
+  return add(num1, num2);
+}
+function add(x, y) {
+  return x + y;
+  }
+  
+function subtract(x, y) {
+  return x - y;
+  }
+  
+function divide(x, y) {
+  return x / y;
+  }
+  
+function multiply(x, y) {
+  return x * y;
+  }
 
-const add = function(x, y) {
-    return x + y;
-  };
-  
-  const subtract = function(x, y) {
-    return x - y;
-  };
-  
-  const sum = function(array) {
-    return array.reduce((total, item) =>{
-      return total + item;
-    }, 0)
-  };
-  
-  const multiply = function(array) {
-    return array.reduce((total, item) =>{
-      return total * item;
-    })
-  };
+function modulo(x, y){
+  return x % y;
+}
+
+function clearCalculator(x, y){
+  return 0;
+}
+
+function equal(fn){
+  return fn;
+}
   
